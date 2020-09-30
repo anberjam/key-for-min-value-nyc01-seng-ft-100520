5bdcf8f11773b07f45a8551dd189b9c123a42e20
Anber Jamshed <anberjam@yahoo.com>
2020-09-30 19:29:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,17 +5,22 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0 
     return nil 
   end
+  
   values=[]
+  
     name_hash.each do |key, value|
       values << [key,value]
     end
+    
   min=values[0][1]
+  index=0
+  
   name_hash.each do |key, value|
     if value < min
       min=value
+      index+=1
     end
   end
-  x=values.index(min)
-  return name_hash[x]
+ return values[index][0]
     
 end
