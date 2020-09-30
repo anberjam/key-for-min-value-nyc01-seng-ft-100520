9559243e3c2f7472a8881fc27ea43a6b5ebf8b22
Anber Jamshed <anberjam@yahoo.com>
2020-09-30 18:56:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0 
+    return nil 
+  end
+  min=0
+  current=0
+  name_hash.each do |key, value|
+    current=value
+    
+    
 end
